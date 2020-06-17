f76dae7a1c8a86b1f8d3af5f92a195598450a065
Rodney Dyer <rjdyer@vcu.edu>
2020-06-17 13:49:11 -0400

    added stubs for all the topic slides and workbooks.

--- R/to_install.R
+++ R/to_install.R
@@ -1,59 +1,65 @@
-# Packages to install.
-rm( list=ls() )
 
+if(0) {
 
-print("Preparing to install packages needed for ENVS543.")
-
-############################################################
-# For Prework
-needed_pkgs  <- c("devtools",
-                  "knitr", "kableExtra", 
-                  "tidyverse",
-                  "rmarkdown","blogdown",
-                  "leaflet", "leaflet.minicharts"
-                  ) 
-
-############################################################
-#Visualization
-needed_pkges <- c( needed_pkgs, 
-                   "ggplot2", "GGally", 
-                   "raster","sp","sf"
-                   ) 
-
-############################################################
-# Data Munging
-needed_pkges <- c( needed_pkgs, 
-                   "reshape2" ) 
-
-
-############################################################
-# Modeling
-needed_pkges <- c( needed_pkgs, 
-                   "lme4" ) 
-
-
-
-
-
-############################################################
-#                                                          #
-#            The following materials are static            #
-#                                                          #
-############################################################
-pkgs <- installed.packages()
-pkgs <- names( is.na(pkgs[,4]))
-to_install <- setdiff( needed_pkges, pkgs )
-
-print( paste("Found", length(to_install), "pakages." ) ) 
-
-for( pkg in to_install) {
-  print( paste("------------------------------  Installing: ", pkg ) )
-  install.packages( pkg )
+  # Packages to install.
+  rm( list=ls() )
+  
+  
+  print("Preparing to install packages needed for ENVS543.")
+  
+  ############################################################
+  # For Prework
+  needed_pkgs  <- c("devtools",
+                    "knitr", "kableExtra", 
+                    "tidyverse",
+                    "rmarkdown","blogdown",
+                    "leaflet", "leaflet.minicharts"
+  ) 
+  
+  ############################################################
+  #Visualization
+  needed_pkges <- c( needed_pkgs, 
+                     "ggplot2", "GGally", 
+                     "raster","sp","sf"
+  ) 
+  
+  ############################################################
+  # Data Munging
+  needed_pkges <- c( needed_pkgs, 
+                     "reshape2" ) 
+  
+  
+  ############################################################
+  # Modeling
+  needed_pkges <- c( needed_pkgs, 
+                     "lme4" ) 
+  
+  
+  
+  
+  
+  ############################################################
+  #                                                          #
+  #            The following materials are static            #
+  #                                                          #
+  ############################################################
+  pkgs <- installed.packages()
+  pkgs <- names( is.na(pkgs[,4]))
+  to_install <- setdiff( needed_pkges, pkgs )
+  
+  print( paste("Found", length(to_install), "pakages." ) ) 
+  
+  for( pkg in to_install) {
+    print( paste("------------------------------  Installing: ", pkg ) )
+    install.packages( pkg )
+  }
+  
+  
+  
+  print(" FINISHED Installing packages for ENVS543")
+  
+  
+  
+  
 }
-
-
-
-print(" FINISHED Installing packages for ENVS543")
-
-
 
